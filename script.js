f829e5a99272d47ed2cee26263b274f2cdcc12f0
IsraelOlayinka <Israel-Force@users.noreply.github.com>
2020-03-31 18:22:23 +0100

    new file:   body.jpg 	new file:   css/style.css 	new file:   css/style.css.map 	deleted:    img1.jpg 	deleted:    img2.jpg 	deleted:    img3.jpg 	deleted:    img4.jpg 	deleted:    img5.png 	deleted:    img6.jpg 	new file:   img7.jpg 	modified:   index.html 	modified:   script.js 	new file:   scss/_body.scss 	new file:   scss/style.css 	new file:   scss/style.css.map 	new file:   scss/style.scss 	deleted:    style-comp.css 	deleted:    style.css

--- script.js
+++ script.js
@@ -1,40 +1,118 @@
- function myFunction() {
-            document.getElementById('myDropdown').classList.toggle("show");
-        }
-        function myFunction1() {
-            document.getElementById('myDropdown1').classList.toggle("show");
-        }function myFunction2() {
-            document.getElementById('myDropdown2').classList.toggle("show");
-        }function myFunction3() {
-            document.getElementById('myDropdown3').classList.toggle("show");
-        }
-        function myFunction4() {
-            document.getElementById('myDropdown4').classList.toggle("show");
-        }
-       window.onclick = function(event) {
-      if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
-      }
-    }
 
-        var today = new Date();
-        var hourNow = today.getHours();
-        var greeting;
-        if (hourNow > 18) {
-            greeting = 'GOOD EVENING';
-        } else if (hourNow > 12) {
-            greeting = 'GOOD AFTERNOON'
-        } else if (hourNow > 0) {
-            greeting = 'GOOD MORNING'
+const btn = document.querySelector('.menu-btn')
+let burger = document.querySelector('.menu-btn_burger')
+let aside = document.querySelector('.navbar')
+let nav = document.querySelector('.nav')
+let content = document.querySelectorAll('.dropdown-content')
+let dropbtn = document.querySelectorAll('.dropbtn')
+let dropdwn = document.querySelectorAll('.dropdown')
+
+/*Burger*/
+btn.addEventListener('click', togglebtn)
+let executed = false
+function togglebtn () {
+  if (!executed) {
+    burger.classList.add('open') 
+    aside.classList.add('open')
+    nav.classList.add('open')  
+    executed = true
+    } else {
+    burger.classList.remove('open')
+        aside.classList.remove('open') 
+        nav.classList.remove('open')
+        executed = false
+    }
+}
+
+/*Dropdown */
+dropdwn.forEach(function (item)
+{ 
+item.addEventListener('click', function () { openDropbtn(this) }) 
+})
+
+//var show = false
+function openDropbtn (btn) {
+  for (var i = 0; i < content.length; i++) {
+    content[i].classList.contains('open')
+    content[i].classList.remove('open')
+  }
+  if (btn) {
+    var nxt = btn.firstChild.nextElementSibling.nextElementSibling;
+      nxt.classList.add('open')
+        //show = true
         } else {
-            greeting = 'WELCOME'
+          var nxt = btn.firstChild.nextElementSibling.nextElementSibling;
+          nxt.classList.remove('open')
+          //show = false;
+        
         }
-        var time = document.getElementById('time')
-        time.innerHTML = greeting
+}
+
+/*window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName('dropdown-content')
+        var i
+        for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i]
+          if (openDropdown.classList.contains('open')) {
+        openDropdown.classList.remove('open')
+          }
+    }
+  }
+}*/
+
+/*Tabbed Panel */
+var tabLinks = document.querySelectorAll('.tablinks')
+
+tabLinks.forEach(function (items) { items.addEventListener('click', function () { openEdu(event, this.textContent) }) })
+
+function openEdu (evt, edu) {
+  var tabcontent = document.getElementsByClassName('tabcontent')
+  for (var i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+  var tablinks = document.getElementsByClassName('tablinks')
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '')
+  }
+  document.getElementById(edu).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
+
+document.getElementById('defaultOpen').click()
+
+
+
+/*Greeting*/
+var today = new Date()
+        var hourNow = today.getHours()
+        var greeting
+        if (hourNow >= 18) {
+  greeting = 'GOOD EVENING!!'
+        } else if (hourNow >= 12) {
+  greeting = 'GOOD AFTERNOON!!'
+} else if (hourNow > 0) {
+  greeting = 'GOOD MORNING!!'
+} else {
+  greeting = 'WELCOME'
+}
+var time = document.getElementById('time')
+time.innerHTML = greeting
+
+
+/*Accordion*/
+
+var acc = document.getElementsByClassName('accordion')
+var i
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    var panel = this.nextElementSibling
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  })
+}
